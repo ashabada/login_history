eb1787a9a5410fb0bcb2570460b57f52bd0f1e5f
user <ashabada@users.noreply.github.com>
2016-02-10 18:56:43 +0200

    Removed unnecessary and incorrect primary key

--- SQL/mysql.initial.sql
+++ SQL/mysql.initial.sql
@@ -1,5 +1,5 @@
 CREATE TABLE login_logs (
-    username VARCHAR(255) NOT NULL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
     remoteip VARCHAR(40) NOT NULL,
     remotedns VARCHAR(255) NOT NULL,
     logintime timestamp NOT NULL
